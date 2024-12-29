4727b6f4dfe68e171b8f57f66c7d40f542b9e5a1
Bhavin <stark.bhavin@gmail.com>
2024-12-29 09:59:05 +0530

    Send Receive Messages

--- components/ChatItem.js
+++ components/ChatItem.js
@@ -1,22 +1,79 @@
-import { View, Text, Image } from "react-native";
-import React from "react";
+import { View, Text } from "react-native";
+
+import React, { useEffect, useState } from "react";
 import { TouchableOpacity } from "react-native";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+import { Image } from "expo-image";
+import blurhash, { formatMessageTime, getRoomID } from "../utils/common";
+import {
+  collection,
+  doc,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import { db } from "../firebaseConfig";
 
-export default function ChatItem({ item, router, noBorder }) {
+export default function ChatItem({ item, router, noBorder, currentUser }) {
+  const [lastMessage, setLastMessage] = useState(undefined);
+
+  useEffect(() => {
+    // Fetches messages
+    let roomId = getRoomID(currentUser?.userId, item?.userId);
+    const docRef = doc(db, "rooms", roomId);
+    const messagesRef = collection(docRef, "messages");
+    const q = query(messagesRef, orderBy("createdAt", "desc"));
+
+    let unsub = onSnapshot(q, (snapshot) => {
+      let allMessages = snapshot.docs.map((doc) => {
+        return doc.data(0);
+      });
+      setLastMessage(allMessages[0] ? allMessages[0] : null);
+    });
+    return unsub;
+  }, []);
+
+  // console.log(lastMessage)
+  const openChatRoom = () => {
+    router.push({ pathname: "/chatRoom", params: item });
+  };
+
+  const renderTime = () => {
+    if (lastMessage) {
+      return formatMessageTime(lastMessage?.createdAt);
+    }
+    return "";
+};
+
+  const renderLastMessage = () => {
+    if (typeof lastMessage == "undefined") {
+      return "Loading...";
+    } else if (lastMessage) {
+      if (currentUser?.userId === lastMessage?.userId) {
+        return "You : " + lastMessage?.text;
+      } else {
+        return lastMessage?.text;
+      }
+    } else {
+      return "Say Hii👋";
+    }
+  };
   return (
     <TouchableOpacity
+      onPress={openChatRoom}
       className={`flex-row justify-between mx-4 items-center gap-3 mb-4 pb-2 ${
         noBorder ? "" : "border-b b-neutral-600"
       }`}
     >
       <Image
-        source={require("../assets/images/favicon.png")}
-        style={{ height: hp(5.5), width: hp(5.5) }}
+        source={item?.profileURL}
+        style={{ height: hp(5.5), width: hp(5.5), borderRadius: 100 }}
         className="rounded-full"
+        placeholder={blurhash}
+        transition={500}
       />
 
       {/* Name and Last Message */}
@@ -26,20 +83,20 @@ export default function ChatItem({ item, router, noBorder }) {
             style={{ fontSize: hp(1.8) }}
             className="font-semibold text-neutral-800"
           >
-            Bhavin
+            {item?.profileName}
           </Text>
           <Text
             style={{ fontSize: hp(1.6) }}
             className="font-medium text-neutral-500"
           >
-            Time
+            {renderTime()}
           </Text>
         </View>
         <Text
           style={{ fontSize: hp(1.6) }}
           className="font-medium text-neutral-500"
         >
-          Last Message
+          {renderLastMessage()}
         </Text>
       </View>
     </TouchableOpacity>
